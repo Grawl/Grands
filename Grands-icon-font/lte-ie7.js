c817c67bdd90904bdfc56ce7b89455443767fe9e
GrawlCore <Grawl@ya.ru>
2012-10-31 20:43:35 +1100

    Revert "added BitBucket icon"
    This reverts commit 988f4332357113c22d73231e97d94bfe816b4722.

--- Grands-icon-font/lte-ie7.js
+++ Grands-icon-font/lte-ie7.js
@@ -43,8 +43,7 @@ window.onload = function() {
 			'icon-Evernote' : '&#xe022;',
 			'icon-Email' : '&#xe023;',
 			'icon-DropBox' : '&#xe024;',
-			'icon-Blogspot' : '&#xe025;',
-			'icon-BitBucket' : '&#xe026;'
+			'icon-Blogspot' : '&#xe025;'
 		},
 		els = document.getElementsByTagName('*'),
 		i, attr, html, c, el;
